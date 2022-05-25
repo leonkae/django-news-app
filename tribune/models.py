2e8c431570f9d715c0925a7ad7be6401af26e4e8
leonkae <leon29kae@gmail.com>
2022-05-25 19:01:26 +0300

    ultered files and downloaded dependecies

--- tribune/models.py
+++ tribune/models.py
@@ -1,3 +1,36 @@
+from re import M 
 from django.db import models
 
-# Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=10, blank=True)
+    
+    def __str__(self):
+        return self.first_name
+    
+    def save_editor(self):
+        return self.save()
+    class Meta:
+        ordering = ['first_name']
+    
+class tags(models.Model):
+    name = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.name
+    
+class Article(models.Model):
+    title = models.CharField(max_length=60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+  
+    
+
+
+
+
+
--- tribune/tests.py
+++ tribune/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
 
-# Create your tests here.
+class EditorTestClass(TestCase):
+    
+    def setUp(self):
+         
+        self.james= Editor(first_name = 'James', last_name = 'Muriuki', email = 'james@moringaschool.com') 
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Editor))
+
+    def test_save_method(self):
+        self.james.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors)>0)
+        
